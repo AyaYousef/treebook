5b9563ac9ad3ca88927e6563f571d085a6d272b6
Youta <youtasweety@gmail.com>
2016-04-11 16:27:14 +0300

    Updated front-end of the new authentication system.
    Added registeration controller and edited the routes so we can access user authenticated info
    Added simple_form
    Updated severl forms to Bootstrap/simple_form
    Added Login/Logout/Signup/Profile links to the nav
    Added a collection to select to the status form.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootst
 #User authentiaction
 gem 'devise'
 
+gem 'simple_form'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :statuses
+  # def fullname method is used to concatenate the first name and the last name together
+  def full_name
+    first_name + " " + last_name
+  end
 end
